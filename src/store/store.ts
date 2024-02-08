8a5a703cfcabd6b7e4b6d04c2f7c5e1bf868564b
Yoav Shati <yoavshati@gmail.com>
2024-02-08 15:51:43 +0200

    feat(store): add slices to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import gameState from './slices/game-state.slice';
+import settings from './slices/settings.slice';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: { gameState, settings },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
